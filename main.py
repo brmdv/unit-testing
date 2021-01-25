2ca98b1e9e5779e4d7abda2f12d03ad066635a84
Bram De Vroey <bramdevroey@gmail.com>
2021-01-25 10:05:57 +0100

    Added unittest test for add()

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 from typing import List, Dict
+import unittest
+
 
 class ConnectionDatabaseError(Exception):
     """Raised when the database connection fails."""
+
     pass
+
 
 class TestDbError(Exception):
     """Raised when a unit test tries to connect to the database."""
+
     pass
 
 
 def connect_to_db(connection_string: str):
     """
     Function that connects to the db.
-    
+
     We will not give you access to the DB yet. So mock this function if you want to test it.
 
     TODO: add a unit test to verify that this function raises a ConnectionDatabaseError when a different connection string than 'test' is provided.
@@ -40,6 +45,32 @@ def get_users_list_from_db(connection_string: str) -> List[Dict[str, str]]:
 
 def add(num_1: int, num_2: int, num_3: int) -> int:
     """
-    TODO: Add a unit test that tests ALL THE INT between 1 and 200. Every possibility should be tested! (your test can't use more than 10 lines)
+    TODO: Add a unit test that tests ALL THE INT between 1 and 200. Every
+    possibility should be tested! (your test can't use more than 10 lines)
     """
     return num_1 + num_2 + num_3
+
+
+# unittest parts
+class TestAddition(unittest.TestCase):
+    """
+    This class contains all tests not related to the database stuff, and that
+    will be tested using the built-in unittest module.
+    """
+
+    def test_many_additions(self):
+        """This tests the add() function with all combinations of 3 numbers
+        between 1 and 200.
+
+        The code doesn't care for commutativity, so many sums will be tested
+        multiple times. The test could obviously be made more efficient when
+        taking this into account, but then again, testing 200³ additions of
+        positive integers is not the most efficient thing in the first place.
+        """
+
+        for a in range(1, 201):
+            for b in range(1, 201):
+                for c in range(1, 201):
+                    self.assertEqual(add(a, b, c), a + b + c)
+
+
